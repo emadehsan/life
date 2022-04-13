5b1f525455401a7166ddb7ac8946a5057891b266
emadehsan <emadulehsan@gmail.com>
2022-04-13 22:29:58 +0500

    show animation, intro, 4 rules

--- app.py
+++ app.py
@@ -9,33 +9,56 @@ from life_game import Life
 CUSTOM_WHITE = '#ecf0f1'
 
 class AppLife(Scene):
+
     def construct(self) -> None:
 
+        # size of each row
+        N = 30
+
+        # this grid will be our playground to display life animation
+        grid = AppLife.create_grid(num_rows=N, num_cols=N, cell_height=10)
+        self.add(grid)
+
+        # create a life object
+        life = Life(num_rows=N, num_cols=N)
+
+        # 1. display laughing head
+        self.show_glider(grid=grid, life=life, num_generations=3, wait_time=0.005)
+
+        # 2. show intro
+        self.show_intro()
+
+        # 3. show rules
+        self.clear()
+        self.add(grid)
+        self.show_rules(grid, life)
+
+
+        # self.embed()
+
+    @staticmethod
+    def create_grid(num_rows, num_cols, cell_height=6):
+        # create a grid of squares
         square = Square()
         square.set_fill(BLACK, opacity=0.6)
         square.set_stroke(BLACK, width=0)
 
-        # size of each row
-        N = 20
+        grid = square.get_grid(num_rows, num_cols, height=cell_height)
+        grid.arrange_in_grid(h_buff=0, v_buff=0)
 
+        return grid
 
-
-        grid = square.get_grid(N, N, height=6)
-        self.add(grid)
-
+    def show_glider(self, grid, life, num_generations, wait_time):
         # create an empty Life board, and then put a glider on it
-        life = Life(num_rows=N, num_cols=N)
+
         life.put_glider_at(5, 5)
+        self.animate_grid(grid, life, num_generations, wait_time)
 
-        # for i in range(10):
-        #     self.wait()
-        #     grid[i].set_fill(CUSTOM_WHITE)
-
-        # return
+    def animate_grid(self, grid, life, num_generations, wait_time=0.1):
 
         generation = 1
-        while generation < 100:
-            self.wait()
+        while generation <= num_generations:
+            self.wait(wait_time)
 
             # put current state on display
             for y in range(life.num_rows):
@@ -47,6 +70,7 @@ class AppLife(Scene):
                     # this cell is alive
                     if life.board[y][x] == 1:
                         grid[grid_idx].set_fill(CUSTOM_WHITE)
+                        # grid[grid_idx].set_fill(WHITE)
                     else:
                         grid[grid_idx].set_fill(BLACK, opacity=0.6)
 
@@ -54,4 +78,73 @@ class AppLife(Scene):
 
             generation += 1
 
-        # self.embed()
+    def show_intro(self):
+        text = Text("Game of Life",
+                    # font="Consolas",
+                    font_size=40)
+
+        body = Text(
+            """
+            Conway's Game of Life is a zero player game\n
+            with 4 simple rules
+            """,
+            font="Arial",
+            font_size=24,
+
+            # t2c is a dict that you can choose color for different text
+            # t2c={"Text": BLUE, "TexText": BLUE, "LaTeX": ORANGE}
+        )
+        VGroup(text, body).arrange(DOWN, buff=1)
+        self.play(Write(text))
+        self.play(Write(body))
+        # self.play(FadeIn(difference, UP))
+        # self.wait(3)
+
+    def show_rules(self, grid, life):
+        rules = [
+            {
+                "title": "1. Underpopulation",
+                "body": "Any live cell with fewer than two live neighbours dies"
+            },
+            {
+                "title": "2. Survival",
+                "body": "Any live cell with two or three live neighbours lives on to the next generation"
+            },
+            {
+                "title": "3. Overpopulation",
+                "body": "Any live cell with more than three live neighbours dies, as if by overpopulation"
+            },
+            {
+                "title": "4. Reproduction",
+                "body": "Any dead cell with exactly three live neighbours becomes a live cell"
+            }
+        ]
+
+        for rule in rules:
+            # clear_grid and text
+
+            self.clear()
+            self.add(grid)
+            # show this rule's animation
+            self.display_underpopulation(grid, life)
+
+            # display this rule's text
+            title = Text(rule['title'], font_size=30, font_color='Red')
+            body = Text(rule['body'], font_size=14)
+            VGroup(title, body).arrange(DOWN, buff=1)
+            self.play(Write(title))
+            self.play(Write(body))
+
+    def display_underpopulation(self, grid, life: Life) -> None:
+        # clear the board
+        life.clear_board()
+
+        # put example in the center
+        x = life.num_cols // 2 - 3
+        y = life.num_rows // 2 - 3
+        life.put_underpopulation_example(x, y)
+
+        # display
+        self.animate_grid(grid, life, num_generations=30)
+        pass
+
--- life_game.py
+++ life_game.py
@@ -1,5 +1,5 @@
-
-from typing import List
+import pprint
+from typing import List, Tuple
 
 
 class Life:
@@ -7,33 +7,27 @@ class Life:
         self.num_rows = num_rows
         self.num_cols = num_cols
         self.board = [
-            [1] * num_cols for _ in range(num_rows)
+            [0] * num_cols for _ in range(num_rows)
         ]
 
-    def clear_board(self):
+    def clear_board(self) -> None:
         for y in range(self.num_rows):
             for x in range(self.num_cols):
                 self.board[y][x] = 0
 
+    def set_board(self, board: List[List[int]]) -> None:
+        # make a deep copy, to avoid bugs?
+        # self.board = [
+        #     row[:] for row in board
+        # ]
+
+        # or manually set the values in old array, to avoid memory leak of pointing to new array
+        for y in range(len(board)):
+            for x in range(len(board[0])):
+                self.board[y][x] = board[y][x]
+
     def get_board(self) -> List[List[int]]:
         return self.board
-
-    def put_glider_at(self, x, y):
-        # will put a glider in the 3x3 box with x,y at its 0,0 position
-        # assumes that board is empty. also direction is bottom-right
-        cells = [
-            # x, y
-            (0, 1),
-            (1, 2),
-            (2, 0),
-            (2, 1),
-            (2, 2)
-        ]
-
-        for cell in cells:
-            x1 = x + cell[0]
-            y1 = y + cell[1]
-            self.board[y1][x1] = 1
 
     def compute_next_state(self) -> None:
         # compute the next state based on the rules of game of life
@@ -75,7 +69,103 @@ class Life:
             x1 = x + neigh[0]
             y1 = y + neigh[1]
 
-            if 0 <= x1 < self.num_cols and 0 <= y1 < self.num_rows:
-                num_neighbours += board_copy[y][x]
+            if 0 <= x1 < len(board_copy[0]) and 0 <= y1 < len(board_copy):
+                num_neighbours += board_copy[y1][x1]
 
         return num_neighbours
+
+    def _make_live(self, cells: List[Tuple[int]], x: int, y: int) -> None:
+        # for the given cell coordinates relative to x,y sets the value of these cells to 1
+        for cell in cells:
+            x1 = x + cell[0]
+            y1 = y + cell[1]
+            self.board[y1][x1] = 1
+
+    def put_glider_at(self, x, y) -> None:
+        # will put a glider in the 3x3 box with x,y at its 0,0 position
+        # assumes that board is empty. also direction is bottom-right
+        cells = [
+            # x, y
+            (0, 1),
+            (1, 2),
+            (2, 0),
+            (2, 1),
+            (2, 2)
+        ]
+
+        self._make_live(cells, x, y)
+
+    def put_underpopulation_example(self, x: int, y: int) -> None:
+        # note: clear the grid before this step
+        # this method puts an L at given x,y
+        # 1
+        # 1
+        # 1, 1
+        live_cells = [
+            # x, y
+            (0, 0),
+            (0, 1),
+            (0, 2),
+            (1, 2)
+        ]
+
+        self._make_live(live_cells, x, y)
+
+    def put_survival_example(self, x: int, y: int) -> None:
+        live_cells = [
+            (0, 0),
+            (0, 1),
+            (1, 1)
+        ]
+        self._make_live(live_cells, x, y)
+
+    def put_overpopulation_example(self, x: int, y: int) -> None:
+        live_cells = [
+            # x, y, according to computer axis?
+            (2, 0),
+            (0, 1),
+            (1, 1),
+            (2, 1),
+            (0, 2)
+        ]
+        self._make_live(live_cells, x, y)
+
+    def put_reproduction_example(self, x: int, y: int) -> None:
+        # 1
+        # 1, 1
+        live_cells = [
+            (0, 0),
+            (0, 1),
+            (1, 1)
+        ]
+        self._make_live(live_cells, x, y)
+
+
+if __name__ == '__main__':
+
+    board = [[0, 1, 0], [0, 0, 1], [1, 1, 1], [0, 0, 0]]
+    # next = [[0,0,0],[1,0,1],[0,1,1],[0,1,0]]
+
+    l = Life(num_rows=len(board), num_cols=len(board[0]))
+    l.set_board(board)
+    print("Original")
+    pprint.pprint(l.get_board())
+
+    l.compute_next_state()
+    print("Next State")
+    pprint.pprint(l.get_board())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
